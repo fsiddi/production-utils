6fe967a32e7bcac88413babd40cc5984758dd4dc
Francesco Siddi <francesco.siddi@gmail.com>
2022-12-24 18:54:58 +0100

    Update script to support PNG images

--- gnuplot_chart.tpl
+++ gnuplot_chart.tpl
@@ -2,20 +2,22 @@ set terminal pngcairo transparent enhanced font "arial,10" fontscale 1.0 size {w
 set output '{tmp_chart_file}'
 set key autotitle columnhead
 
-
 set multiplot
 set size 1, 0.5
 
-# set ytics offset 4,0
+#set tics in
+#unset tics
+#set tic scale 0
+set ytics offset 2,0.5
 
 # Display border
 set border lt 4
-unset border
+# unset border
 
-# Set fixed tics
-set xtics 2
+# Set fixed ticks
+set xtics 20
 
-# Set y axis to start a 0
+# Set y axis to start at 0
 set yrange [0:*]
 
 # Set x axis to start at frame number
@@ -23,17 +25,12 @@ set xrange [{frame_start_number}:*]
 
 set origin 0.0,0.5
 # unset xtics
-plot '{frames_stats_file}' using 1:3 with lines lt 4
+# set y2label "speed" textcolor lt 4
+set key textcolor variable
+plot '{frames_stats_file}' using 1:3 with lines lt 4 title 'Memory (MB)'
 
 set origin 0.0,0.0
-
-
-plot '{frames_stats_file}' using 1:4 with lines lt 4
+plot '{frames_stats_file}' using 1:4 with lines lt 4 title 'Render time (m)'
 
 unset multiplot
-
-# set xtics in
-#unset xtics
-#set border "white"
-#plot 'data.dat' with lines
 
--- shotstats.py
+++ shotstats.py
@@ -18,6 +18,7 @@ parser.add_argument('-i', '--in_path', help='Input path', required=True)
 parser.add_argument('-o', '--out_path', help='Output directory')
 parser.add_argument('-y', '--skip_confirmation', help='Skip confirmation', action='store_true')
 parser.add_argument('-f', '--framerate', help='Framerate', default=24)
+parser.add_argument('--image_format', help='Either PNG or EXR', default='PNG')
 parser.add_argument('--memory_unit', default='G')
 parser.add_argument('--render_time_unit', help='How display render time', default='m')
 args = parser.parse_args()
@@ -45,18 +46,25 @@ toolset = {
 # If exr, use exrheader (we will also need jpeg previews later)
 
 
-def parse_metadata(s):
-    result = re.search('"(.*)"', s)
-    return result.group(1)
+def parse_metadata(metadata_string: str):
+    if args.image_format == 'EXR':
+        # Metadata looks like this:
+        # Memory (type string): "0.00M"
+        re_result = re.search('"(.*)"', metadata_string)
+        return re_result.group(1)
+    elif args.image_format == 'PNG':
+        # Metadata looks like this:
+        #     Memory: 0.00M
+        return metadata_string.split(':', 1)[1].strip()
 
 
-def parse_memory(s):
+def parse_memory(memory: str):
     """Get the amount of memory used.
 
-    We strip the last char, and assume it's M. The we cast to float.
+    We strip the last char, and assume it's M. Then we cast to float.
     """
-    s = parse_metadata(s)
-    memory_in_mb = float(s[:-1])
+    memory = parse_metadata(memory)
+    memory_in_mb = float(memory[:-1])
     if args.memory_unit == 'G':
         m = memory_in_mb / 1024
     else:
@@ -64,12 +72,12 @@ def parse_memory(s):
     return m
 
 
-def parse_render_time(s):
+def parse_render_time(time_metadata):
     """Get the render time in seconds."""
-    s = parse_metadata(s)
-    time_array = s.split(':')
+    time_metadata = parse_metadata(time_metadata)
+    time_array = time_metadata.split(':')
     if len(time_array) < 2:  # Only seconds
-        time_in_seconds = float(s)
+        time_in_seconds = float(time_metadata)
     elif len(time_array) < 3:  # Minutes and seconds
         time_in_seconds = int(time_array[0]) * 60 + float(time_array[1])
     elif len(time_array) < 4:  # Hours, minutes and seconds
@@ -84,14 +92,14 @@ def parse_render_time(s):
     return t
 
 
-def parse_frame_number(s):
+def parse_frame_number(frame_number_metadata):
     """Get the frame number."""
-    s = parse_metadata(s)
-    return int(s)
+    frame_number_metadata = parse_metadata(frame_number_metadata)
+    return int(frame_number_metadata)
 
 
 def parse_exr_frames(frames_list):
-    """Parse EXR frames using the exrheader command."""
+    """Parse EXR frames using the 'exrheader' command."""
     frames_stats = []
     for frame in frames_list:
         exrheader_command = [
@@ -114,7 +122,34 @@ def parse_exr_frames(frames_list):
 
     return frames_stats
 
+
 # If png use identify -verbose
+def parse_png_frames(frames_list):
+    frames_stats = []
+    for frame in frames_list:
+        identify_command = [
+            toolset['identify'],
+            '-verbose',
+            frame
+        ]
+        p = subprocess.Popen(identify_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        lines = iter(out.decode('utf-8').splitlines())
+        frame_stats = {'name': frame.stem}
+        for line in lines:
+            line = line.strip()
+            if line.startswith('Memory'):
+                frame_stats['memory_in_mb'] = parse_memory(line)
+            elif line.startswith('RenderTime'):
+                frame_stats['render_time_in_s'] = parse_render_time(line)
+            # elif line.startswith('Frame'):
+            #     frame_stats['frame_number'] = parse_frame_number(line)
+            frame_stats['frame_number'] = int(frame.stem)
+        print(f"Frame {frame_stats['frame_number']}: {frame_stats['memory_in_mb']} - {frame_stats['render_time_in_s']}")
+        frames_stats.append(frame_stats)
+
+    return frames_stats
+
 
 # Get current directory
 cwd = Path.cwd()
@@ -122,30 +157,45 @@ cwd = Path.cwd()
 # Get absolute path of input dir (if relative it will be combined with cwd)
 in_dir_absolute_path = cwd.joinpath(args.in_path)
 
+# Look for files (png or exr)
+frames = sorted(in_dir_absolute_path.glob(f'*.{args.image_format.lower()}'))
+
 frames_stats_path = in_dir_absolute_path / 'frames_stats.csv'
 
-# Look for exr files
-frames = sorted(in_dir_absolute_path.glob('*.exr'))
-if frames:
-    stats = parse_exr_frames(frames)
-    with open(frames_stats_path, 'w', newline='') as csvfile:
-        fieldnames = ['frame_number', 'name', 'memory_in_mb', 'render_time_in_s']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
 
-        writer.writeheader()
-        for s in stats:
-            writer.writerow(s)
-        print(f'{frames_stats_path} is ready.')
-else:
-    # TODO(fsiddi) Handle PNG images
-    print('No images found.')
-    sys.exit()
+def get_frame_stats():
+    if frames_stats_path.exists():
+        with open(frames_stats_path) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter='\t')
+            return [row for row in reader]
+    if frames:
+        if args.image_format == 'EXR':
+            stats = parse_exr_frames(frames)
+        elif args.image_format == 'PNG':
+            stats = parse_png_frames(frames)
+        with open(frames_stats_path, 'w', newline='') as csvfile:
+            fieldnames = ['frame_number', 'name', 'memory_in_mb', 'render_time_in_s']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter='\t')
 
+            writer.writeheader()
+            for s in stats:
+                writer.writerow(s)
+            print(f'{frames_stats_path} is ready.')
+            return stats
+    else:
+        print(f'No {args.image_format} images found.')
+        sys.exit()
+
+
+stats = get_frame_stats()
 # Get frame resolution
 
-# If we are working with exr, look for a .jpg file
 # TODO(fsiddi) handle missing image
-first_frame = frames[0].with_suffix('.jpg')
+first_frame = frames[0]
+# If we are working with exr, look for a .jpg file
+if args.image_format == 'EXR':
+    first_frame = frames[0].with_suffix('.jpg')
+
 identify_format = '%[fx:w]x%[fx:h]'
 
 identify_command = [
@@ -192,17 +242,16 @@ gnuplot_command = [
 
 subprocess.call(gnuplot_command)
 
+sys.exit()
 
 # Combine the chart with images sequence and overlay the playhead
-
-
 # For instance if we have a 260 frames clip
 #
 # 260 frames = 260 / <args.framerate> = 10.83333 seconds
 # video_width / video_duration = 2048 / 10.83333 = 189.04621 pixels / second.
 
 # Arbitrary offset defined by the chart
-chart_margin_x_pixel = 32
+chart_margin_x_pixel = 29
 
 chart_width = int(frame_width) - chart_margin_x_pixel
 
@@ -213,8 +262,11 @@ overlay_string = f"overlay, overlay=x='if(gte(t,0), -w+{chart_margin_x_pixel}+(t
 # Get the number of the first frame of the sequence
 start_number = stats[0]['frame_number']
 
-output_file = f'{in_dir_absolute_path.name}-{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.mp4'
-input_path = in_dir_absolute_path.joinpath('%6d.jpg')  # TODO(fsiddi) also support PNG extension
+output_file_name = f'{in_dir_absolute_path.name}-{datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")}.mp4'
+output_file_path = Path(args.out_path) / output_file_name
+
+extension = 'png' if args.image_format == 'PNG' else 'jpg'
+input_path = in_dir_absolute_path.joinpath(f'%6d.{extension}')
 
 ffmpeg_command = [
     'ffmpeg',
@@ -230,7 +282,7 @@ ffmpeg_command = [
     'playhead.png',
     '-filter_complex',
     f'{overlay_string}',
-    output_file
+    str(output_file_path)
 ]
 
 subprocess.call(ffmpeg_command)
